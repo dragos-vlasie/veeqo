1b16a272ac3d989a7b5a200407e39f3158b9dcda
dragos-vlasie <37800900+dragos-vlasie@users.noreply.github.com>
2024-04-17 22:04:03 +0100

    add button styles

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,11 +1,11 @@
-import clsx from 'clsx';
-import Link from 'next/link';
+import clsx from "clsx";
+import Link from "next/link";
 
 type ButtonProps = {
   children: React.ReactNode;
   href: string;
   className?: string;
-}
+};
 
 /**
  * Common Button component
@@ -14,17 +14,10 @@ type ButtonProps = {
  * @param {*} props Additional props to pass onto the button
  * @returns <Button href={href} variant={variant} className={className} {...props}>{children}<Button />
  */
-export default function Button({
-  children,
-  href,
-  className,
-  ...props
-}: ButtonProps) {
-  return <Link 
-  className={
-    clsx('w-fit h-fit py-2 px-3 rounded-3xl ', className)
-  }
-  href={href}
-  {...props}
-  >{children}</Link>
+export default function Button({ children, href, className, ...props }: ButtonProps) {
+  return (
+    <Link className={clsx("w-fit h-fit py-3 px-4 rounded-xl font-semibold bg-[#05192D] text-white ", className)} href={href} {...props}>
+      {children}
+    </Link>
+  );
 }
