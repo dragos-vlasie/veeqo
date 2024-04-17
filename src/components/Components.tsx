5104d1c87407ccef7eae5b5593d84ecd3dce4147
dragos-vlasie <37800900+dragos-vlasie@users.noreply.github.com>
2024-04-17 19:26:58 +0100

    add props for components

--- src/components/Components.tsx
+++ src/components/Components.tsx
@@ -1,22 +1,31 @@
-import React from "react";
+import React, { FC } from "react";
 import IntroImagesSection from "./IntroImageSection";
 import Section from "./Section";
 import IconsText from "./IconsText";
 import Header from "./Header";
 
-const Components = {
+// Define the types for item props
+type ItemProps = {
+  contentType: string;
+  padding?: string;
+  theme?: string;
+  content?: any[];
+};
+
+// Define the Components object with appropriate types
+const Components: { [key: string]: FC<any> } = {
   IntroImagesSection,
   Section,
   IconsText,
   Header
-  // Add more components as needed
 };
 
-export const renderComponent = (item: any) => {
-    const Component = Components[item.contentType];
-    if (Component) {
-      return <Component {...item} />;
-    } else {
-      return <div>The component {item.contentType} has not been created yet.</div>;
-    }
-  };
+// Define the renderComponent function with proper types
+export const renderComponent = (item: ItemProps) => {
+  const Component = Components[item.contentType];
+  if (Component) {
+    return <Component {...item} />;
+  } else {
+    return <div>The component {item.contentType} has not been created yet.</div>;
+  }
+};
