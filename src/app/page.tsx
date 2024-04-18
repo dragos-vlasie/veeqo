e2a3df187f2d70c55795bf32e48a35612172ee75
dragos-vlasie <37800900+dragos-vlasie@users.noreply.github.com>
2024-04-18 12:30:14 +0100

    update metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,6 @@
 import { MockData } from "../data";
 import { renderComponent } from "@/components/Components";
 import { Fragment } from "react";
-import { Metadata } from 'next'
-
-export const metadata: Metadata = {
-  metadataBase: new URL('http://localhost:3000/'), //add actual metadata
-  title: "Veeqo",
-  description: "Interview task",
-  openGraph: {
-    title: "Veeqo task",
-    description: "Social media description",
-    images: [
-      {
-        url: "/assets/featured-image-1.png",
-        width: 800,
-        height: 600,
-        alt: "Featured Image",
-      },
-    ],
-  },
-};
-
-
 
 export default function Home() {
   return (
